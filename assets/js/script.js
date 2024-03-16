a400cbaba743b3427c4d41fb7d79451f57c51f55
Faris <kittyowayanad@gmail.com>
2024-03-17 02:13:48 +0530

    Some CSS fixes

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    $('#name').on('input',function(e) {
+        console.log("Updatedt");
+        let message = getWhatsappMessage
+        if(message != '') {
+            $("#message").val(message);
+        }
+    });
+
+    
+
+})
+
+
+function getWhatsappMessage() {
+    let name = $("#name").val();
+    let message = "";
+    if (name) { 
+        message = "Hi, I am " + name + "!, and I am reaching out to make an order. Could you kindly provide more details on this?";
+    }
+    return message;
+}
+
+function chatOnWhatsapp() { 
+    let name = $("#name").val(); 
+
+    if (!name) { 
+        alert("Name is required"); 
+        return; 
+    }
+
+    let content = getWhatsappMessage(); 
+    let whatsapp_url = "https://wa.me/919656472000?text=" + encodeURI(content); 
+    window.open(whatsapp_url, '_blank'); 
+}
